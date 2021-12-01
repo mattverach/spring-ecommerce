7027512f7d56703f6898e8b3b59f0a41fe76e989
Matías Vera <matias_vera@outlook.com>
2021-12-01 02:54:01 -0300

    Agregué las relaciones entre tablas y vinculé la aplicación con mi base de datos

--- src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
+++ src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
@@ -2,10 +2,8 @@ package com.curso.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.dao.DataAccessResourceFailureException;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class SpringEcommerceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/curso/ecommerce/model/DetalleOrden.java
+++ src/main/java/com/curso/ecommerce/model/DetalleOrden.java
@@ -1,11 +1,28 @@
 package com.curso.ecommerce.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "detalles")
 public class DetalleOrden {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private double cantidad;
 	private double precio;
 	private double total;
+	
+	@OneToOne
+	private Orden orden;
+	
+	@OneToOne
+	private Producto producto;
 	
 	public DetalleOrden() {
 		// TODO Auto-generated constructor stub
@@ -58,6 +75,24 @@ public class DetalleOrden {
 
 	public void setTotal(double total) {
 		this.total = total;
+	}
+
+	
+	
+	public Orden getOrden() {
+		return orden;
+	}
+
+	public void setOrden(Orden orden) {
+		this.orden = orden;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
 	}
 
 	@Override
