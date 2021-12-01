2e764b107955cd71ecb9fd0e637e7b6533b9889f
Matías Vera <matias_vera@outlook.com>
2021-12-01 01:45:13 -0300

    Creacion de clases model

---
+++ src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
@@ -0,0 +1,15 @@
+package com.curso.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.dao.DataAccessResourceFailureException;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SpringEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcommerceApplication.class, args);
+	}
+
+}
