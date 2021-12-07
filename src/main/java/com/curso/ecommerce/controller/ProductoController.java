24db4eaa96b421f3c1ecd8389a2f087eea39f523
Matías Vera <matias_vera@outlook.com>
2021-12-07 03:24:31 -0300

    Added CreateProduct, AdminMenu and logger
    I defined the method to create a product but haven't implemented it yet.
    
    Added AdminMenu and linked all of the pages using the navbar.
    
    Added a logger to check how the object "producto" worked

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -1,16 +1,35 @@
 package com.curso.ecommerce.controller;
 
+import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.curso.ecommerce.model.Producto;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 	
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+	
 	@GetMapping("")
 	public String show() {
 		return "productos/show";
 	}
+	
+	@GetMapping("/create")
+	public String create() {
+		return "productos/create";
+	}
+	
+	@PostMapping("/save")
+	public String save(Producto producto) {
+		LOGGER.info("Este es el objeto producto {}",producto);
+		return "redirect:/productos";
+	}
+	
+	
 
 }
